6218660fde8b6638ce0a9e9df36eaf2536b35a52
thomasjfcaldwell <thomasjfcaldwell@gmail.com>
2020-09-25 13:08:03 -0700

    navbar

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {Container, Navbar, } from 'react-router-bootstrap'
+
+
+
+class Header extends Component {
+	render() {
+		return (
+			<Container>
+				<Navbar expand='lg' variant='light' bg='light'>
+					
+				</Navbar>
+			</Container>
+		);
+	}
+}
+
+export default Header;

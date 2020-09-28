b6a3902e7c06172a379532bdc74c00fb1876ad43
thomasjfcaldwell <thomasjfcaldwell@gmail.com>
2020-09-28 10:58:51 -0700

    new navbar

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
-import {Container, Navbar, } from 'react-router-bootstrap'
-
-
+import { Nav, Navbar,NavDropdown} from 'react-bootstrap';
 
 class Header extends Component {
 	render() {
 		return (
-			<Container>
-				<Navbar expand='lg' variant='light' bg='light'>
-					
-				</Navbar>
-			</Container>
+			<Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+				<Navbar.Brand href='#home'>Thomas Caldwell Portfolio</Navbar.Brand>
+				<Navbar.Toggle aria-controls='responsive-navbar-nav' />
+				<Navbar.Collapse id='responsive-navbar-nav'>
+					<Nav className='mr-auto'>
+						<Nav.Link href='#features'>contact</Nav.Link>
+						<Nav.Link href='#pricing'>About Thomas</Nav.Link>
+						<NavDropdown title='Projects' id='collasible-nav-dropdown'>
+							<NavDropdown.Item href='#action/3.1'>Project 1</NavDropdown.Item>
+							<NavDropdown.Item href='#action/3.2'>Project 2</NavDropdown.Item>
+							<NavDropdown.Item href='#action/3.3'>Project 3</NavDropdown.Item>
+						</NavDropdown>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
 		);
 	}
 }

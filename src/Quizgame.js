cf3f9a7a815f2c597e06bcd31ad03de935a2ef89
thomasjfcaldwell <thomasjfcaldwell@gmail.com>
2021-09-29 10:21:36 -0700

    more changes

--- src/Quizgame.js
+++ src/Quizgame.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Row, Col, Container, Image } from 'react-bootstrap';
-import data from './data/data.json';
+import { Row, Col, Container, Image, Card } from 'react-bootstrap';
+import ProjectsData from './data/data.json';
 import '../src/KnowYourWizard.scss';
 
 class QuizGame extends Component {
@@ -8,7 +8,24 @@ class QuizGame extends Component {
 		return (
 			<>
 				<div>
-					
+					{ProjectsData.map((ProjectsInfo, id) => {
+						return (
+							<>
+								<Container>
+									<Row>
+										<Col>
+											<Card.Title>{ProjectsInfo.title}</Card.Title>
+											<Image src={ProjectsInfo.title} />
+										</Col>
+										<Col>
+											<Card.Text>{ProjectsInfo.intro}</Card.Text>
+											<Card.Text>{ProjectsInfo.technologies}</Card.Text>
+										</Col>
+									</Row>
+								</Container>
+							</>
+						);
+					})}
 				</div>
 			</>
 		);

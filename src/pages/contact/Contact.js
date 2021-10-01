8f766dc0019afec26f32dae129525199f593174c
thomasjfcaldwell <thomasjfcaldwell@gmail.com>
2021-09-30 22:44:21 -0700

    skills updated

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Form, Col, Row } from 'react-bootstrap';
+import { Container, Form, Card, Col, Row } from 'react-bootstrap';
 import '../../style/home.css';
 
 class Contact extends Component {
@@ -10,40 +10,41 @@ class Contact extends Component {
 					<div className='headline-container'>
 						<h1 className='headline'>Contact Thomas Here</h1>
 					</div>
-
-					<Form
-						action='https://docs.google.com/forms/u/0/d/e/1FAIpQLSdYWaw0WkN_qpVogvapyKaZqx9A_hqGmmMWrWsYIPSziWcBRg/formResponse'
-						method='POST'>
-						<div>
-							<input
-								name='entry.1673834915'
-								type='text'
-								className='feedback-input'
-								required
-								placeholder='Name'
-							/>
-						</div>
-						<div>
-							<input
-								name='entry.218700094'
-								type='text'
-								className='feedback-input'
-								required
-								placeholder='Email'
-							/>
-						</div>
-						<div>
-							<textarea
-								name='entry.1359360774'
-								className='feedback-input'
-								placeholder=' send a message here'
-								style={{
-									color: 'black',
-								}}
-							/>
-						</div>
-						<input type='submit' value='Submit'></input>
-					</Form>
+					<Card>
+						<Form
+							action='https://docs.google.com/forms/u/0/d/e/1FAIpQLSdYWaw0WkN_qpVogvapyKaZqx9A_hqGmmMWrWsYIPSziWcBRg/formResponse'
+							method='POST'>
+							<div>
+								<input
+									name='entry.1673834915'
+									type='text'
+									className='feedback-input'
+									required
+									placeholder='Name'
+								/>
+							</div>
+							<div>
+								<input
+									name='entry.218700094'
+									type='text'
+									className='feedback-input'
+									required
+									placeholder='Email'
+								/>
+							</div>
+							<div>
+								<textarea
+									name='entry.1359360774'
+									className='feedback-input'
+									placeholder=' send a message here'
+									style={{
+										color: 'black',
+									}}
+								/>
+							</div>
+							<input type='submit' value='Submit'></input>
+						</Form>
+					</Card>
 				</Container>
 			</div>
 		);
